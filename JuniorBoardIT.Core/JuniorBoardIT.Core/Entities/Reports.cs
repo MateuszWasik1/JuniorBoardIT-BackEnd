932a0bd58a9dd49d7566eaafe595b6ea20144542
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-07-17 17:19:06 +0200

    1.30 - Entity & Enum

---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/Entities/Reports.cs
@@ -0,0 +1,20 @@
+﻿using JuniorBoardIT.Core.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Organiser.Cores.Entities
+{
+    public class Reports
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RID { get; set; }
+        public Guid RGID { get; set; }
+        public int RUID { get; set; }
+        public string? RAUIDS { get; set; }
+        public DateTime RDate { get; set; }
+        public string? RReasons { get; set; }
+        public string? RText { get; set; }
+        public ReportsStatusEnum? RStatus { get; set; }
+    }
+}
---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/Models/Enums/ReportsStatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace JuniorBoardIT.Core.Models.Enums
+{
+    public enum ReportsStatusEnum
+    {
+        New = 0,
+        InVerification = 1,
+        Rejected = 2,
+        Accepted = 3,
+        Fixed = 4,
+    }
+}
