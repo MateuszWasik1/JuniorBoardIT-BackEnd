51ff66bc8753562ef46f9a2f28d12bbbdc3f33ab
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-07-23 17:42:58 +0200

    1.30 Controllers & Entities

---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/Controllers/BugsNotesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using JuniorBoardIT.Core.CQRS.Dispatcher;
+using JuniorBoardIT.Core.CQRS.Resources.Bugs.BugsNotes.Commands;
+using JuniorBoardIT.Core.CQRS.Resources.Bugs.BugsNotes.Queries;
+using JuniorBoardIT.Core.Models.ViewModels.BugsViewModels;
+
+namespace JuniorBoardIT.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BugsNotesController : ControllerBase
+    {
+        private readonly IDispatcher dispatcher;
+        public BugsNotesController(IDispatcher dispatcher) => this.dispatcher = dispatcher;
+
+        [HttpGet]
+        [Route("GetBugNotes")]
+        public GetBugsNotesViewModel GetBugNotes(Guid bgid, int skip, int take)
+            => dispatcher.DispatchQuery<GetBugNotesQuery, GetBugsNotesViewModel>(new GetBugNotesQuery() { BGID = bgid, Skip = skip, Take = take });
+
+        [HttpPost]
+        [Route("SaveBugNote")]
+        public void SaveBugNote(BugsNotesViewModel model)
+            => dispatcher.DispatchCommand(new SaveBugNoteCommand() { Model = model });
+    }
+}
