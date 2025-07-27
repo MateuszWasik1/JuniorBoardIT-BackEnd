e0e969b1a0d8ba237130e8f7718eff08acb1a35e
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-07-27 13:01:15 +0200

    1.37 - Add NIP/REGON/KRS to data

--- JuniorBoardIT.Core/JuniorBoardIT.Core/Entities/Companies.cs
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/Entities/Companies.cs
@@ -19,6 +19,9 @@ namespace JuniorBoardIT.Core.Entities
         public string? CCountry { get; set; }
         public string? CPostalCode { get; set; }
         public string? CPhoneNumber { get; set; }
+        public string? CNIP { get; set; }
+        public string? CRegon { get; set; }
+        public string? CKRS { get; set; }
         public string? CLI { get; set; }
         public int CFoundedYear { get; set; }
         public CompanyEmpNoEnum CEmployeesNo { get; set; }
