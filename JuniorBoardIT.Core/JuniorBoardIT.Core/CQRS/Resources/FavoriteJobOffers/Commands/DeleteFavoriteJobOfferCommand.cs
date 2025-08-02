7a1ee0bff8e13597b52eade4390c34c8fe98011a
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-08-02 15:30:20 +0200

    1.32 - FavoriteJobOffers CQRS

---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/CQRS/Resources/FavoriteJobOffers/Commands/DeleteFavoriteJobOfferCommand.cs
@@ -0,0 +1,9 @@
+﻿using JuniorBoardIT.Core.CQRS.Abstraction.Commands;
+
+namespace JuniorBoardIT.Core.CQRS.Resources.FavoriteJobOffers.Commands
+{
+    public class DeleteFavoriteJobOfferCommand : ICommand
+    {
+        public Guid FJOGID { get; set; }
+    }
+}
---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/CQRS/Resources/FavoriteJobOffers/Handlers/AddFavoriteJobOfferCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using JuniorBoardIT.Core.Context;
+using JuniorBoardIT.Core.CQRS.Abstraction.Commands;
+using JuniorBoardIT.Core.CQRS.Resources.FavoriteJobOffers.Commands;
+using JuniorBoardIT.Core.Services;
+
+namespace JuniorBoardIT.Core.CQRS.Resources.FavoriteJobOffers.Handlers
+{
+    public class AddFavoriteJobOfferCommandHandler : ICommandHandler<AddFavoriteJobOfferCommand>
+    {
+        private readonly IDataBaseContext context;
+        private readonly IUserContext user;
+        public AddFavoriteJobOfferCommandHandler(IDataBaseContext context, IUserContext user)
+        {
+            this.context = context;
+            this.user = user;
+        }
+        public void Handle(AddFavoriteJobOfferCommand command)
+        {
+
+            var model = new Entities.FavoriteJobOffers()
+            {
+                FJOGID = Guid.NewGuid(),
+                FJOUGID = Guid.Parse(user.UGID),
+                FJOJOGID = command.JOGID
+            };
+
+            context.CreateOrUpdate(model);
+            context.SaveChanges();
+        }
+    }
+}
