c41714d849dc475b3ee7c1aa6e8b4b3301aca6f3
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-08-07 21:04:54 +0200

    1.49 - Stats - CQRS

---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/CQRS/Resources/Stats/Queries/GetNumberOfActiveCompaniesOffertsQuery.cs
@@ -0,0 +1,10 @@
+﻿using JuniorBoardIT.Core.Models.ViewModels.StatsViewModels;
+using JuniorBoardIT.Core.CQRS.Abstraction.Queries;
+
+namespace JuniorBoardIT.Core.CQRS.Resources.Stats.Queries
+{
+    public class GetNumberOfActiveCompaniesOffertsQuery : IQuery<StatsBarChartViewModel>
+    {
+        public DateTime Date { get; set; }
+    }
+}
