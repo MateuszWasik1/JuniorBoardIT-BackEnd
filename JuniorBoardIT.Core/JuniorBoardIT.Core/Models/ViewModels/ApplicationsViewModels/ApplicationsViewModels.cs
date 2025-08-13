731dd0ce1d56d58fa6704d479c7db41d0c82928a
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-08-13 16:51:41 +0200

    1.62 - Entity & ViewModels

---
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/Models/ViewModels/ApplicationsViewModels/ApplicationsViewModels.cs
@@ -0,0 +1,8 @@
+﻿namespace JuniorBoardIT.Core.Models.ViewModels.ApplicationsViewModels
+{
+    public class ApplicationsViewModels
+    {
+        public List<ApplicationViewModels> List { get; set; }
+        public int Count { get; set; }
+    }
+}
