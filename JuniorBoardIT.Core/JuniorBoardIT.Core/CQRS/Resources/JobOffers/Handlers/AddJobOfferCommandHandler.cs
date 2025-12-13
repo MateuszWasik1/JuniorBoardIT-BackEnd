003e1c15c39da391e12783b0ecf7fae5343cb13d
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-12-13 20:16:33 +0100

    Delete unused code

--- JuniorBoardIT.Core/JuniorBoardIT.Core/CQRS/Resources/JobOffers/Handlers/AddJobOfferCommandHandler.cs
+++ JuniorBoardIT.Core/JuniorBoardIT.Core/CQRS/Resources/JobOffers/Handlers/AddJobOfferCommandHandler.cs
@@ -16,27 +16,6 @@ namespace JuniorBoardIT.Core.CQRS.Resources.JobOffers.Handlers
         }
         public void Handle(AddJobOfferCommand command)
         {
-            //if (command.Model.UUserName.Length == 0)
-            //    throw new UserNameRequiredException("Nazwa użytkownika nie może być pusta!");
-
-            //if (command.Model.UUserName.Length > 100)
-            //    throw new UserNameMax100Exception("Nazwa użytkownika nie może mieć więcej niż 100 znaków!");
-
-            //if (command.Model.UFirstName.Length > 50)
-            //    throw new UserFirstNameMax50Exception("Imię użytkownika nie może mieć więcej niż 50 znaków!");
-
-            //if (command.Model.ULastName.Length > 50)
-            //    throw new UserLastNameMax50Exception("Nazwisko użytkownika nie może mieć więcej niż 50 znaków!");
-
-            //if (command.Model.UEmail.Length == 0)
-            //    throw new UserEmailRequiredException("Email użytkownika nie może być pusty!");
-
-            //if (command.Model.UEmail.Length > 100)
-            //    throw new UserEmailMax100Exception("Email użytkownika nie może mieć więcej niż 100 znaków!");
-
-            //if (command.Model.UPhone.Length > 100)
-            //    throw new UserPhoneMax100Exception("Telefon użytkownika nie może mieć więcej niż 100 znaków!");
-
             var model = new Entities.JobOffers()
             {
                 JOGID = Guid.NewGuid(),
