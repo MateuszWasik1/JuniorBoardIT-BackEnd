799585e74aa559cf8bc8a379b7178caab587243d
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-12-21 17:31:02 +0100

    test: 3.99 - Report Queries UnitTests

---
+++ JuniorBoardIT.Core/JuniorBoardIT.UnitTests/CQRS/QueryHandler/Reports/TestGetReportQueryHandler.cs
@@ -0,0 +1,151 @@
+﻿using AutoMapper;
+using JuniorBoardIT.Core.Context;
+using JuniorBoardIT.Core.CQRS.Resources.Reports.Handlers;
+using JuniorBoardIT.Core.CQRS.Resources.Reports.Queries;
+using JuniorBoardIT.Core.Exceptions.JobOffers;
+using JuniorBoardIT.Core.Exceptions.Reports;
+using JuniorBoardIT.Core.Models.ViewModels.CompaniesViewModel;
+using JuniorBoardIT.Core.Models.ViewModels.JobOffersViewModels;
+using JuniorBoardIT.Core.Models.ViewModels.ReportsViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace JuniorBoardIT.UnitTests.CQRS.QueryHandler.Reports
+{
+    [TestFixture]
+    public class TestGetReportQueryHandler
+    {
+        private Mock<IDataBaseContext> context;
+        private Mock<IMapper> mapper;
+
+        private List<Core.Entities.Reports> reports;
+        private List<Core.Entities.JobOffers> jobOffers;
+        private List<Core.Entities.Companies> companies;
+
+        private Guid reportGid;
+        private Guid jobOfferGid;
+        private Guid companyGid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new Mock<IDataBaseContext>();
+            mapper = new Mock<IMapper>();
+
+            reportGid = Guid.NewGuid();
+            jobOfferGid = Guid.NewGuid();
+            companyGid = Guid.NewGuid();
+
+            reports = new List<Core.Entities.Reports>
+            {
+                new Core.Entities.Reports
+                {
+                    RGID = reportGid,
+                    RJOGID = jobOfferGid
+                }
+            };
+
+            jobOffers = new List<Core.Entities.JobOffers>
+            {
+                new Core.Entities.JobOffers
+                {
+                    JOGID = jobOfferGid,
+                    JOCGID = companyGid
+                }
+            };
+
+            companies = new List<Core.Entities.Companies>
+            {
+                new Core.Entities.Companies
+                {
+                    CGID = companyGid
+                }
+            };
+
+            context.Setup(x => x.Reports).Returns(reports.AsQueryable());
+            context.Setup(x => x.JobOffers).Returns(jobOffers.AsQueryable());
+            context.Setup(x => x.Companies).Returns(companies.AsQueryable());
+
+            mapper.Setup(x => x.Map<Core.Entities.Reports, ReportsViewModel>(It.IsAny<Core.Entities.Reports>())).Returns(new ReportsViewModel());
+
+            mapper.Setup(x => x.Map<Core.Entities.JobOffers, JobOfferViewModel>(It.IsAny<Core.Entities.JobOffers>())).Returns(new JobOfferViewModel { JOCGID = companyGid });
+
+            mapper.Setup(x => x.Map<Core.Entities.Companies, CompanyViewModel>(It.IsAny<Core.Entities.Companies>())).Returns(new CompanyViewModel());
+        }
+
+        [Test]
+        public void TestGetReportQueryHandler_ReportNotFound_ShouldThrow_ReportNotFoundExceptions()
+        {
+            // Arrange
+            var query = new GetReportQuery { RGID = Guid.NewGuid() };
+            var handler = new GetReportQueryHandler(context.Object, mapper.Object);
+
+            // Act
+            // Assert
+            Assert.Throws<ReportNotFoundExceptions>(() => handler.Handle(query));
+        }
+
+        [Test]
+        public void TestGetReportQueryHandler_JobOfferNotFound_ShouldThrow_JobOfferNotFoundExceptions()
+        {
+            // Arrange
+            jobOffers.Clear();
+
+            var query = new GetReportQuery { RGID = reportGid };
+            var handler = new GetReportQueryHandler(context.Object, mapper.Object);
+
+            // Act
+            // Assert
+            Assert.Throws<JobOfferNotFoundExceptions>(() => handler.Handle(query));
+        }
+
+        [Test]
+        public void TestGetReportQueryHandler_CompanyExists_ShouldReturn_FullModel()
+        {
+            // Arrange
+            var query = new GetReportQuery { RGID = reportGid };
+            var handler = new GetReportQueryHandler(context.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ReportModel, Is.Not.Null);
+            Assert.That(result.JobOfferModel, Is.Not.Null);
+            Assert.That(result.CompanyModel, Is.Not.Null);
+        }
+
+        [Test]
+        public void TestGetReportQueryHandler_CompanyNotFound_ShouldReturn_EmptyCompanyModel()
+        {
+            // Arrange
+            companies.Clear();
+
+            var query = new GetReportQuery { RGID = reportGid };
+            var handler = new GetReportQueryHandler(context.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.CompanyModel, Is.Not.Null);
+        }
+
+        [Test]
+        public void TestGetReportQueryHandler_ShouldMap_AllEntities()
+        {
+            // Arrange
+            var query = new GetReportQuery { RGID = reportGid };
+            var handler = new GetReportQueryHandler(context.Object, mapper.Object);
+
+            // Act
+            handler.Handle(query);
+
+            // Assert
+            mapper.Verify(x => x.Map<Core.Entities.Reports, ReportsViewModel>(It.IsAny<Core.Entities.Reports>()), Times.Once);
+            mapper.Verify(x => x.Map<Core.Entities.JobOffers, JobOfferViewModel>(It.IsAny<Core.Entities.JobOffers>()), Times.Once);
+            mapper.Verify(x => x.Map<Core.Entities.Companies, CompanyViewModel>(It.IsAny<Core.Entities.Companies>()), Times.Once);
+        }
+    }
+}
---
+++ JuniorBoardIT.Core/JuniorBoardIT.UnitTests/CQRS/QueryHandler/Reports/TestGetReportsQueryHandler.cs
@@ -0,0 +1,213 @@
+﻿using AutoMapper;
+using JuniorBoardIT.Core.Context;
+using JuniorBoardIT.Core.CQRS.Resources.Reports.Handlers;
+using JuniorBoardIT.Core.CQRS.Resources.Reports.Queries;
+using JuniorBoardIT.Core.Models.Enums;
+using JuniorBoardIT.Core.Models.ViewModels.ReportsViewModels;
+using JuniorBoardIT.Core.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace JuniorBoardIT.UnitTests.CQRS.QueryHandler.Reports
+{
+    [TestFixture]
+    public class TestGetReportsQueryHandler
+    {
+        private Mock<IDataBaseContext> context;
+        private Mock<IMapper> mapper;
+        private Mock<IUserContext> userContext;
+
+        private List<Core.Entities.Reports> reports;
+        private List<Core.Entities.User> users;
+
+        private Guid userGid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new Mock<IDataBaseContext>();
+            mapper = new Mock<IMapper>();
+            userContext = new Mock<IUserContext>();
+
+            userGid = Guid.NewGuid();
+
+            reports = new List<Core.Entities.Reports>()
+            {
+                new Core.Entities.Reports
+                {
+                    RID = 1,
+                    RSupportGID = null,
+                    RStatus = ReportsStatusEnum.New,
+                    RText = "Pierwszy raport",
+                    RDate = DateTime.UtcNow.AddDays(-2)
+                },
+                new Core.Entities.Reports
+                {
+                    RID = 2,
+                    RSupportGID = userGid,
+                    RStatus = ReportsStatusEnum.InVerification,
+                    RText = "Drugi raport",
+                    RDate = DateTime.UtcNow.AddDays(-1)
+                },
+                new Core.Entities.Reports
+                {
+                    RID = 3,
+                    RSupportGID = Guid.NewGuid(),
+                    RStatus = ReportsStatusEnum.InVerification,
+                    RText = "Trzeci raport",
+                    RDate = DateTime.UtcNow
+                }
+            };
+
+            users = new List<Core.Entities.User>()
+            {
+                new Core.Entities.User
+                {
+                    UID = 1,
+                    UGID = userGid,
+                    URID = (int) RoleEnum.Support
+                }
+            };
+
+            context.Setup(x => x.Reports).Returns(reports.AsQueryable());
+            context.Setup(x => x.User).Returns(users.AsQueryable());
+
+            userContext.Setup(x => x.UID).Returns(1);
+            userContext.Setup(x => x.UGID).Returns(userGid.ToString());
+
+            mapper.Setup(x => x.Map<Core.Entities.Reports, ReportsViewModel>(It.IsAny<Core.Entities.Reports>())).Returns<Core.Entities.Reports>(r => new ReportsViewModel { RID = r.RID });
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_ReportTypeNew_ShouldReturn_OnlyNewReports()
+        {
+            // Arrange
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.New,
+                Skip = 0,
+                Take = 10
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(1));
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result.List[0].RID, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_ReportTypeImVerificator_ShouldReturn_AssignedReports()
+        {
+            // Arrange
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.ImVerificator,
+                Skip = 0,
+                Take = 10
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(1));
+            Assert.That(result.List[0].RID, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_ReportTypeAll_Admin_ShouldReturn_AllReports()
+        {
+            // Arrange
+            users[0].URID = (int) RoleEnum.Admin;
+
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.All,
+                Skip = 0,
+                Take = 10
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(3));
+            Assert.That(result.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_ReportTypeAll_NotAdmin_ShouldReturn_FilteredReports()
+        {
+            // Arrange
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.All,
+                Skip = 0,
+                Take = 10
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(1));
+            Assert.That(result.List[0].RID, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_WithMessageFilter_ShouldReturn_MatchingReports()
+        {
+            // Arrange
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.All,
+                Message = "Drugi",
+                Skip = 0,
+                Take = 10
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(1));
+            Assert.That(result.List[0].RID, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestGetReportsQueryHandler_ShouldApply_SkipAndTake()
+        {
+            // Arrange
+            users[0].URID = (int) RoleEnum.Admin;
+
+            var query = new GetReportsQuery
+            {
+                ReportType = ReportsTypeEnum.All,
+                Skip = 1,
+                Take = 1
+            };
+
+            var handler = new GetReportsQueryHandler(context.Object, userContext.Object, mapper.Object);
+
+            // Act
+            var result = handler.Handle(query);
+
+            // Assert
+            Assert.That(result.List.Count, Is.EqualTo(1));
+            Assert.That(result.Count, Is.EqualTo(3));
+        }
+    }
+}
